920ecd6cafe7c4d793871da9b827b055544ccfde
Rafael Takaka <tsws@Rafaels-iMac.local>
2018-01-18 16:26:34 +0100

    Initial Commit

---
+++ Sklep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sklep
+//
+//  Created by Rafael Takaka on 18.01.2018.
+//  Copyright © 2018 Rafael Takaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
